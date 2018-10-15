9625722e3eeb30d4095314db70b7b6f399d526d3
Dominik Harmim <xharmi00@stud.fit.vutbr.cz>
2018-10-15 02:23:30 +0200

    Lab 3

---
+++ 3 WPF/Windows/MainWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+using WPFWeather.Services;
+using WPFWeather.ViewModels;
+
+namespace WPFWeather.Windows
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = new WeatherViewModel(new WeatherOpenWeatherMapService());
+        }
+    }
+}
